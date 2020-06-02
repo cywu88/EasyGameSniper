a2e6ef66029aa1f455963fb67a561318ed685f0f
easygamesniper <cywu88@126.com>
2020-06-02 08:06:50 +0800

    增加一些基础类

---
+++ FootStone/SGFCore/NetWork/FSPLite/Client/FSPClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGF.NetWork.FSPLite.Client
+{
+    public class FSPClient
+    {
+        private bool m_IsRunning = false;
+
+
+
+    }
+}
